03ce72696e293fcb069723b2f4e720d3e62e6737
bmigo <berniecmigo@gmail.com>
2023-02-15 19:37:52 -0500

    added helper

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers }); //NOT DEFINED
+const hbs = exphbs.create({helpers}); //not defined
 
 const sess = {
   secret: 'Super secret secret',
